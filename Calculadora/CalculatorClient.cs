97f012aac749c596f97a28e3be1a58df45f47f2b
tomasQL <tomasgaticagomez@gmail.com>
2024-09-29 15:14:07 -0300

    Primer commit

---
+++ Calculadora/CalculatorClient.cs
@@ -0,0 +1,6 @@
+namespace Calculadora;
+
+public class CalculatorClient
+{
+    
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1. Menú con selección de opciones como: SUMAR, RESTAR, MULTIPLICAR, DIVIDIR,POTENCIA, SALIR.
+// 2. El menú debe permanecer en un ciclo WHILE y debe terminar sólo si el usuario selecciona la
+// opción salir.
+// 3. Si el usuario selecciona alguna opción que no se encuentre en el menú, el programa debe
+// mostrar un mensaje indicando que la opción no es válida.
+// 4. Todas las opciones deben ser métodos de una clase llamada calculadora y las variables
+// deben ser sus atributos.
+// 5. Debe validar en el caso de la división que el divisor no sea cero y volver a solicitar al usuario
+// el ingreso de este valor.
+// 6. El usuario debe ser capaz de seleccionar cuántos números desea utilizar en las opciones
+// SUMAR, RESTAR y MULTIPLICAR.
+// 7. Cree el diagrama de clases correspondiente a la aplicación desarrollada.
+// 8. Cargue el desarrollo de la aplicación y el diagrama en respuesta a esta actividad.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+
+
